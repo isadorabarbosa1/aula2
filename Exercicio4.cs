a7b314c47aeb09a82fc92a3112602e827e282a2f
isadorabarbosa1 <isadora.cardoso@edu.senai.br>
2026-02-19 14:38:33 -0300

    Entrega EX4, 5, 6 e 7

---
+++ Exercicio4.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public static class Exercicio4
+{
+    public static void Executar()
+
+    { 
+         string nome = "Isadora Barbosa";
+    int idade = 16;
+    string hobby1 = "treinar";
+    string hobby2 = "dancar";
+    string graduaçao = "odontologia";
+      Console.WriteLine($"meu nome é {nome}; eu tenho {idade} meus hobbes é {hobby1} e {hobby2} e minha graduacao é {graduaçao}");
+    }
+}
---
+++ Exercicio5.cs
@@ -0,0 +1,12 @@
+public static class Exercicio5
+{
+    public static void Executar()
+    {
+        string nome = "mulher";
+        string estilo = "sertanejo";
+        string artista = "henrique e juliano";
+        Console.WriteLine($"eu sou {nome}; e curto o estilo musical {estilo} e meu artista favorito {artista}");
+
+    }
+   
+}
